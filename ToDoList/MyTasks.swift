ab0539eb152b1844cba2925b5d72c3c2fc71443e
izudilov <mrzudilov@mail.ru>
2021-04-05 23:01:46 +0200

    HT-5

---
+++ ToDoList/MyTasks.swift
@@ -0,0 +1,15 @@
+//
+//  MyTasks.swift
+//  ToDoList
+//
+//  Created by user179996 on 05.04.2021.
+//
+
+import Foundation
+
+class MyTasks {
+        
+    static let shared = MyTasks()
+    private init () {}
+        
+}
